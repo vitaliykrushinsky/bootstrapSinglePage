16edb66d91386cfc9d083ff51c2e2b92f026de14
Vitaliy Krushinsky <vitaliykrushinsky@gmail.com>
2015-07-27 18:14:10 +0300

    Added scroll smoothly feature

--- js/myscript.js
+++ js/myscript.js
@@ -29,6 +29,22 @@ $(function() {
             $('header nav').removeClass('inbody');
         }
     });
+	
+	//Use smooth scrolling when clicking on navigation
+	$('.navbar a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//,'') === 
+		  this.pathname.replace(/^\//,'') && 
+		  location.hostname === this.hostname) {
+		  var target = $(this.hash);
+		  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		  if (target.length) {
+			$('html,body').animate({
+			  scrollTop: target.offset().top-topoffset+2
+			}, 500);
+			return false;
+		  } //target.length
+		} //click function
+  	}); //smooth scrolling
     
 });
 
